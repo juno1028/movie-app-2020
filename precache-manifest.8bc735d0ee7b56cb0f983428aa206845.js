a3a6e579dfe88e9f35bb1fe5a9b362cb19f0794a
Juno_Kim <juno9325@gmail.com>
2020-08-12 11:52:59 +0900

    Updates

---
+++ precache-manifest.8bc735d0ee7b56cb0f983428aa206845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f40235326b5e8852d2c7eb955c1679",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c3f95a8ad072f23f7aec",
+    "url": "/movie_app_2020/static/css/main.196d3b4e.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f95a8ad072f23f7aec",
+    "url": "/movie_app_2020/static/js/main.66be86fe.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
